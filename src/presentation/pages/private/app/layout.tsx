948dc8fb5c676c2e8433c01f19c0abad607ea4f9
Willian Igor <willianigordeveloper@gmail.com>
2024-01-31 20:45:50 -0300

    :construction: brainstorming of app layout

--- src/presentation/pages/private/app/layout.tsx
+++ src/presentation/pages/private/app/layout.tsx
@@ -1,5 +1,139 @@
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/presentation/components/ui/avatar";
 import { AppProps } from ".";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/presentation/components/ui/carousel";
+import { Input } from "@/presentation/components/ui/input";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/presentation/components/ui/card";
+import { Button } from "@/presentation/components/ui/button";
+import { Filter } from "lucide-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/presentation/components/ui/dialog";
 
 export function AppLayout({}: AppProps) {
-  return <></>;
+  return (
+    <>
+      <header className="flex justify-between items-center p-3 border-b border-zinc-700 bg-zinc-900">
+        <div>
+          <h1>Study</h1>
+        </div>
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>U</AvatarFallback>
+        </Avatar>
+      </header>
+      <main>
+        <section>
+          <Carousel>
+            <CarouselContent>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+                  <div className="p-3">
+                    <div className="flex aspect-video items-center justify-center p-6 border border-zinc-700 rounded-lg">
+                      <span className="text-3xl font-semibold">
+                        Card {index + 1}
+                      </span>
+                    </div>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+          </Carousel>
+        </section>
+        <section className="p-3 space-y-3">
+          <div className="flex gap-3 items-center">
+            <div className="max-w-60">
+              <Input
+                className="border-zinc-700"
+                placeholder="Search for something..."
+              />
+            </div>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button size="sm" className="gap-2">
+                  <Filter size={14} /> Filters
+                </Button>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Are you absolutely sure?</DialogTitle>
+                  <DialogDescription>
+                    This action cannot be undone. This will permanently delete
+                    your account and remove your data from our servers.
+                  </DialogDescription>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog>
+          </div>
+          <Card>
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+        </section>
+      </main>
+    </>
+  );
 }
